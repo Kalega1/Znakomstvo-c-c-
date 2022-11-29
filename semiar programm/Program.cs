881fc8dae1a70776aeb575419fb3ea1592df22f4
Kalega1 <kalegin.m@yandex.ru>
2022-11-30 00:15:04 +0500

    расчехлил комментs

--- semiar programm/Program.cs
+++ semiar programm/Program.cs
@@ -1,2 +1,44 @@
-﻿int a = 5, b = 3;
-Console.WriteLine($"My number is {a} and  {b} it is good");
+﻿//Домашняя работа 1.
+
+//Задача 1. Написать программу которая принимает два числа
+
+Console.Write("Ввести первое число: ");
+int num1 = Convert.ToInt32(Console.ReadLine());
+Console.Write("Ввести второе число: ");
+int num2 = Convert.ToInt32(Console.ReadLine());
+
+int quad = num2 * num2;
+if(num1 == quad)
+    Console.WriteLine("Да");
+else
+    Console.WriteLine("Нет");
+
+
+//Задача 2.
+
+Console.Write("Ввести ввести целое число: ");
+int num = Convert.ToInt32(Console.ReadLine());
+int current = -num;
+while(current <= num)
+{
+    Console.Write(current + " ");
+    current++;
+}
+
+12345 / 10 = 1234
+12345 / 100 = 123
+12345 / 1000 = 12
+12345 / 10000 = 1
+
+12345 % 10 = 5
+12345 % 100 = 45
+12345 % 1000 = 345
+12345 % 10000 = 2345
+12345 % 100000 = 12345
+
+// Домашняя работа
+
+
+
+
+
