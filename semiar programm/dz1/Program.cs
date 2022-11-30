f48afb0aa1da359fb66850cec16de3d13f23b767
Kalega1 <kalegin.m@yandex.ru>
2022-11-30 12:26:05 +0500

    первое задание выполнено

---
+++ semiar programm/dz1/Program.cs
@@ -0,0 +1,16 @@
+﻿//Урок 1.
+
+//Задача 2. Напишите программу, которая на вход принимает два числа и выдаёт, какое число большее, а какое меньшее.
+
+/*Console.Write("Ввести первое число: ");
+int num1 = Convert.ToInt32(Console.ReadLine());
+Console.Write("Ввести второе число: ");
+int num2 = Convert.ToInt32(Console.ReadLine());
+
+if(num1 > num2)
+    Console.WriteLine(num1 + " = макс, " + num2 + " = мин. ");
+if(num1 < num2)
+    Console.WriteLine(num2 + " = макс, " + num1 + " = мин. ");
+    */
+
+    
