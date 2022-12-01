f3a5f9f495980e121518a635e509742508c00f9b
Kalega1 <kalegin.m@yandex.ru>
2022-12-01 10:28:32 +0500

    сделал второе домашнее задание

--- semiar programm/dz1/Program.cs
+++ semiar programm/dz1/Program.cs
@@ -13,4 +13,19 @@ if(num1 < num2)
     Console.WriteLine(num2 + " = макс, " + num1 + " = мин. ");
     */
 
-    
+Console.Write("Введите первое число: ");
+int num1 = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите второе число: ");
+int num2 = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите третье число: ");
+int num3 = Convert.ToInt32(Console.ReadLine());
+
+if(num1 > num2 & num1 > num3)
+    Console.WriteLine(num1 + " = макс");
+else 
+    if(num2 > num1)
+        Console.WriteLine(num2 + " = макс");
+    else
+        Console.WriteLine("они все равны");
+Console.ReadKey();
+
